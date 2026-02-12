f2b5e7b0fca3058cea00694ab0301fb54fbc86d1
qtfpsymnwc-wq <256923820+qtfpsymnwc-wq@users.noreply.github.com>
2026-02-11 20:11:15 -0600

    Update app.js

--- ui/app.js
+++ ui/app.js
@@ -1,6 +1,6 @@
 // FlightsAboveMe UI
 const API_BASE = "https://flightsabove.t2hkmhgbwz.workers.dev";
-const UI_VERSION = "v183";
+const UI_VERSION = "v184";
 
 // Poll cadence
 const POLL_MAIN_MS = 8000;
