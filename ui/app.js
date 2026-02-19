1266e9c426bad37d2cbec2534bb8d1534cd87f5f
qtfpsymnwc-wq <256923820+qtfpsymnwc-wq@users.noreply.github.com>
2026-02-18 23:15:33 -0600

    Add files via upload

--- ui/app.js
+++ ui/app.js
@@ -1,7 +1,7 @@
 // FlightsAboveMe UI
 const API_BASE = window.location.origin;
 // Cache-buster for static assets (CSS/JS/logos)
-const UI_VERSION = "v234";
+const UI_VERSION = "v235";
 
 // Poll cadence
 const POLL_MAIN_MS = 8000;
